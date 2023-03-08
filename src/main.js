ff72350afd6ddf09b17d1f7c6591f2e347fd13ea
Jaron Armiger <jaron.armiger@gmail.com>
2023-03-07 23:54:47 -0800

    build text roll anim

--- src/main.js
+++ src/main.js
@@ -1,8 +1,52 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
 import './styles/style.css'
+import gsap from 'gsap'
+import $ from 'jquery'
+import SplitType from 'split-type'
 
-console.log('Welcome to Vite + JS + Webflow!')
+console.log(gsap)
+console.log($)
+console.log(SplitType)
 
-createBadge()
-animateTitle()
+// split type
+
+let typeSplit = new SplitType('.work_title', {
+  types: 'words, chars',
+  tagName: 'span',
+})
+
+console.log(typeSplit)
+
+$('.work_link').each(function () {
+  let firstChars = $(this).find('.work_title.is-1 .char')
+  let secondChars = $(this).find('.work_title.is-2 .char')
+
+  let tl = gsap.timeline({ paused: true })
+  tl.to(firstChars, {
+    translateY: '-0.2em',
+    rotationY: '-5.7deg',
+    rotationX: '-90deg',
+    stagger: { each: 0.08 },
+    ease: 'power3.inOut',
+    duration: 0.7,
+  })
+  tl.from(
+    secondChars,
+    {
+      translateY: '0.2em',
+      rotationY: '5.7deg',
+      rotationX: '90deg',
+      stagger: { each: 0.08 },
+      ease: 'power3.inOut',
+      duration: 0.7,
+    },
+    0.1
+  )
+
+  $(this).on('mouseenter', function () {
+    tl.restart()
+  })
+
+  $(this).on('mouseleave', function () {
+    tl.reverse()
+  })
+})
